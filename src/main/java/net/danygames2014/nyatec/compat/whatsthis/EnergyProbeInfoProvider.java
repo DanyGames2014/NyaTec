b4b4d8adb59b9f08fc5afb473371367685d6c13f
Daniel Fildán <fildand@centrum.cz>
2025-05-11 21:03:18 +0200

    Add energy flow to Whats This Probe Info

--- src/main/java/net/danygames2014/nyatec/compat/whatsthis/EnergyProbeInfoProvider.java
+++ src/main/java/net/danygames2014/nyatec/compat/whatsthis/EnergyProbeInfoProvider.java
@@ -1,7 +1,12 @@
 package net.danygames2014.nyatec.compat.whatsthis;
 
 import net.danygames2014.nyalib.energy.EnergyStorage;
+import net.danygames2014.nyalib.network.Network;
+import net.danygames2014.nyalib.network.NetworkManager;
+import net.danygames2014.nyalib.network.NetworkType;
+import net.danygames2014.nyalib.network.energy.EnergyNetwork;
 import net.danygames2014.nyatec.NyaTec;
+import net.danygames2014.nyatec.block.CableBlockTemplate;
 import net.danygames2014.whatsthis.api.IProbeHitData;
 import net.danygames2014.whatsthis.api.IProbeInfo;
 import net.danygames2014.whatsthis.api.IProbeInfoProvider;
@@ -23,13 +28,30 @@ public class EnergyProbeInfoProvider implements IProbeInfoProvider {
         
         if(world.getBlockEntity(pos.x, pos.y, pos.z) instanceof EnergyStorage energyStorage) {
             probeInfo.progress(
-                    energyStorage.getEnergyStored(), 
-                    energyStorage.getEnergyCapacity(), 
-                    probeInfo.defaultProgressStyle().suffix(" EU")
-                    .borderColor(0xFFF5A9B8)
-                    .filledColor(0xFF5BCEFA)
-                    .alternateFilledColor(0xFFFFFFFF)
+                energyStorage.getEnergyStored(), 
+                energyStorage.getEnergyCapacity(), 
+                probeInfo.defaultProgressStyle().suffix(" EU")
+                .borderColor(0xFFF5A9B8)
+                .filledColor(0xFF5BCEFA)
+                .alternateFilledColor(0xFFFFFFFF)
             );
+        }
+        
+        if(world.getBlockState(pos).getBlock() instanceof CableBlockTemplate) {
+            Network net = NetworkManager.getAt(world.dimension, pos.x, pos.y, pos.z, NetworkType.ENERGY.getIdentifier());
+            if (net instanceof EnergyNetwork energyNet) {
+                EnergyNetwork.EnergyFlowEntry flowEntry = energyNet.getFlowEntry(pos.x, pos.y, pos.z);
+                if (flowEntry != null) {
+                    probeInfo.progress(
+                        flowEntry.energyFlow, 
+                        flowEntry.conductor.getBreakdownPower(world, flowEntry.componentEntry),
+                        probeInfo.defaultProgressStyle().suffix(" EU/t")
+                        .borderColor(0xFFF5A9B8)
+                        .filledColor(0xFF5BCEFA)
+                        .alternateFilledColor(0xFFFFFFFF)
+                    );
+                }
+            }
         }
     }
 }
