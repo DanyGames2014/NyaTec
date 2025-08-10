86af5c56e0aca21d2ee854e5742e25a4f31256db
Daniel Fildán <fildand@centrum.cz>
2025-08-11 00:31:42 +0200

    Slot Types ?

--- src/main/java/net/danygames2014/nyatec/block/entity/SlotType.java
+++ src/main/java/net/danygames2014/nyatec/block/entity/SlotType.java
@@ -2,7 +2,9 @@ package net.danygames2014.nyatec.block.entity;
 
 public enum SlotType {
     INPUT,
-    OUTPUT,
+    OUTPUT_PRIMARY,
+    OUTPUT_SECONDARY,
+    OUTPUT_WASTE,
     FUEL,
     OTHER;
 }
