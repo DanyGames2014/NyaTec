79778b7e3502186c94507aca54db737759a77904
Daniel Fildán <fildand@centrum.cz>
2025-08-12 22:54:17 +0200

    Energy Storage Block tooltip

--- src/main/java/net/danygames2014/nyatec/block/entity/BaseEnergyStorageBlockEntity.java
+++ src/main/java/net/danygames2014/nyatec/block/entity/BaseEnergyStorageBlockEntity.java
@@ -1,8 +1,11 @@
 package net.danygames2014.nyatec.block.entity;
 
 import net.danygames2014.nyatec.block.entity.template.EnergySourceConsumerBlockEntityTemplate;
+import net.minecraft.item.ItemStack;
+import net.modificationstation.stationapi.api.client.item.CustomTooltipProvider;
+import org.jetbrains.annotations.NotNull;
 
-public abstract class BaseEnergyStorageBlockEntity extends EnergySourceConsumerBlockEntityTemplate {
+public abstract class BaseEnergyStorageBlockEntity extends EnergySourceConsumerBlockEntityTemplate implements CustomTooltipProvider {
     int size;
 
     public BaseEnergyStorageBlockEntity(int size) {
@@ -13,5 +16,16 @@ public abstract class BaseEnergyStorageBlockEntity extends EnergySourceConsumerB
     @Override
     public int getEnergyCapacity() {
         return size;
+    }
+
+    @Override
+    public @NotNull String[] getTooltip(ItemStack stack, String originalTooltip) {
+        return new String[]{
+                originalTooltip,
+                "Maximum Voltage Input: " + getMaxInputVoltage(null) + " EU",
+                "Maximum Energy Input: " + getMaxEnergyInput(null) + " EU/t",
+                "Maximum Voltage Output: " + getMaxOutputVoltage(null) + " EU",
+                "Maximum Energy Output: " + getMaxEnergyOutput(null) + " EU/t"
+        };
     }
 }
