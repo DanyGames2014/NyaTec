2702daa03e1f9c9329870368a04f9b8bf2940766
Daniel Fildán <fildand@centrum.cz>
2025-08-13 09:29:30 +0200

    Allow macerator recipe to have custom recipeTime

--- src/main/java/net/danygames2014/nyatec/recipe/MaceratorRecipe.java
+++ src/main/java/net/danygames2014/nyatec/recipe/MaceratorRecipe.java
@@ -1,4 +1,10 @@
 package net.danygames2014.nyatec.recipe;
 
-public class MaceratorRecipe extends MachineRecipe{
+public class MaceratorRecipe extends MachineRecipe {
+    public MaceratorRecipe(int recipeTime) {
+        super(recipeTime);
+    }
+
+    public MaceratorRecipe() {
+    }
 }
