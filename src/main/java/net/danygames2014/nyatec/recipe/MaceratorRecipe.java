3568ba4c98e92d003e457b1b8dccb9fa1ec77bbd
Daniel Fildán <fildand@centrum.cz>
2025-11-08 15:51:17 +0100

    More accurate name of the property

--- src/main/java/net/danygames2014/nyatec/recipe/MaceratorRecipe.java
+++ src/main/java/net/danygames2014/nyatec/recipe/MaceratorRecipe.java
@@ -1,8 +1,8 @@
 package net.danygames2014.nyatec.recipe;
 
 public class MaceratorRecipe extends MachineRecipe {
-    public MaceratorRecipe(int recipeTime) {
-        super(recipeTime);
+    public MaceratorRecipe(int requiredEnergy) {
+        super(requiredEnergy);
     }
 
     public MaceratorRecipe() {
